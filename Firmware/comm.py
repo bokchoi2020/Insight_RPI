493772eba3bba567d8074581546b1f3640a24cbc
bokchoi2020 <byluo@uwaterloo.ca>
2018-07-10 23:11:47 -0400

    Add uuid to comm.py
    Advertise uuid for Android app connection.
    Use any bluetooth port instead of just port 1.
    Will need to test to see if it works.

--- Firmware/comm.py
+++ Firmware/comm.py
@@ -1,11 +1,21 @@
 import bluetooth
  
 server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
- 
-port = 1
-server_socket.bind(("",port))
+uuid = ac9635ee-84a7-11e8-adc0-fa7ae01bbebc 
+#port = 1
+#server_socket.bind(("",port))
+server_sock.bind(("", bluetooth.PORT_ANY))
 server_socket.listen(1)
- 
+port = server_sock.getsockname()[1]
+
+bluetooth.advertise_service( server_socket, "SampleServer",
+                        service_id = uuid,
+                        service_classes = [ uuid, bluetooth.SERIAL_PORT_CLASS ],
+                        profiles = [ bluetooth.SERIAL_PORT_PROFILE ] 
+                         )
+
+print "Waiting for connection on RFCOMM channel %d" % port
+
 client_socket,address = server_socket.accept()
 print "Accepted connection from ",address
 while 1:
